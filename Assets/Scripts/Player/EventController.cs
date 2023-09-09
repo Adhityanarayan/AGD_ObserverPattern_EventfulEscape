897ce4f9c508124391b26fba3df33f1e6edbb951
Adhitya <45228715+Adhityanarayan@users.noreply.github.com>
2023-09-09 12:01:15 +0530

    Applying Event Architecture for LightSwitch Event

--- Assets/Scripts/Player/EventController.cs
+++ Assets/Scripts/Player/EventController.cs
@@ -3,7 +3,6 @@
 public class EventController
 {
     public event Action baseEvent;
-
     public void AddListener(Action listener) => baseEvent += listener;
     public void RemoveListener(Action listener) => baseEvent -= listener;
     public void InvokeEvent() => baseEvent?.Invoke();
